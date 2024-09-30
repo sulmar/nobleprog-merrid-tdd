00dd431096ec0fe5db0059927941baeb93fccefc
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-09-30 10:43:12 +0200

    Dodanie testów wypozyczenia

---
+++ src/TestApp.UnitTests/RentTests.cs
@@ -0,0 +1,37 @@
+namespace TestApp.UnitTests
+{
+    [TestClass]
+    public class RentTests
+    {
+
+        [TestMethod]
+        public void Method_Scenario_ExpectedBehavior()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CanReturn_UserIsAdmin_ShouldReturnsTrue()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CanReturn_UserIsEmpty_ShouldThrowsArgumentNullException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CanReturn_UserIsRenteeAndIsNotAdmin_ShouldReturnsTrue()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CanReturn_UserIsNotRenteeAndIsNotAdmin_ShouldReturnsFalse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
