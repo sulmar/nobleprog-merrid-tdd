06e6fa40dcbbad8b25cad9caafbe34a8fbfcbe36
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-09-30 16:04:10 +0200

    Dodanie przykladu testowania void

---
+++ src/TestApp.xUnitTests/LoggerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.xUnitTests
+{
+    public class LoggerTests
+    {
+        const string MessageNotEmpty = "a";
+
+        [Fact]
+        public void Log_MessageNotEmpty_ShouldSetLastMessage()
+        {
+            // Arrange
+            Logger logger = new Logger();
+
+            // Act
+            logger.Log(MessageNotEmpty);
+
+            // Assert
+            Assert.Equal(MessageNotEmpty, logger.LastMessage);
+        }
+    }
+}
