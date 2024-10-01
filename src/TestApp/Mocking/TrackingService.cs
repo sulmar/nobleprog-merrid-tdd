af4568ea49f5c061ca3ba5b020d09e1854eaf9e3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-01 11:11:54 +0200

    Umożliwienie testowania

--- src/TestApp/Mocking/TrackingService.cs
+++ src/TestApp/Mocking/TrackingService.cs
@@ -4,11 +4,51 @@ using System.Text.Json;
 
 namespace TestApp.Mocking
 {
+    public interface IFileReader
+    {
+        string Get(string path);
+    }
+
+    public class FileReader : IFileReader
+    {
+        public string Get(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+
+    public class FakeFileReader : IFileReader
+    {
+        public string Get(string path)
+        {
+            string json = @"{
+                  ""location"": {
+                    ""latitude"": 40.712776,
+                    ""longitude"": -74.005974
+                  }
+                }";
+
+            return json;
+        }
+    }
+
     public class TrackingService
     {
+        private IFileReader fileReader;
+
+        public TrackingService()
+        {
+            this.fileReader = new FileReader();
+        }
+
+        public TrackingService(IFileReader fileReader)
+        {
+            this.fileReader = fileReader;
+        }
+
         public Location Get()
         {
-            string json = File.ReadAllText("tracking.txt");
+            string json = fileReader.Get("tracking.txt");
 
             Location location = JsonSerializer.Deserialize<Location>(json);
 
