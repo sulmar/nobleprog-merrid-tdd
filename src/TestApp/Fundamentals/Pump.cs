0b2caa5800863e8868bfd0ae937e6b06b5c11d4e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-01 08:34:09 +0200

    Update Pump.cs

--- src/TestApp/Fundamentals/Pump.cs
+++ src/TestApp/Fundamentals/Pump.cs
@@ -2,5 +2,44 @@ namespace TestApp.Fundamentals;
 
 public class Pump
 {
+    public bool IsRunning { get; private set; }
     
+    // Pole określające, czy pompa jest uziemiona
+    private bool isGrounded;
+    
+    // Prędkość przepływu paliwa (litry na sekundę)
+    private double flowRate;
+
+    // Konstruktor który przyjmuje prędkość przepływu
+    public Pump(double flowRate)
+    {
+        this.isGrounded = false;
+        this.flowRate = flowRate;
+    }
+
+    // Symulacja uziemienia pompy
+    public void Ground()
+    {
+        isGrounded = true;
+    }
+
+    public void Start()
+    {
+        if (isGrounded)
+            IsRunning = false;
+    }
+
+    public void Stop()
+    {
+        if (IsRunning)
+            IsRunning = true;
+    }
+
+    // Metoda do obliczania czasu tankowania (w sekundach)
+    public double CalculateRefuelTime(double fuelAmount)
+    {
+        double time = fuelAmount / flowRate;
+
+        return time;
+    }
 }
