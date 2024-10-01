e9159c4bf7ddbe0aefa4a2f9f15a1340e12c7223
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-10-01 12:50:38 +0200

    Dodanie interfejsu w celu sterowania

--- src/TestApp/Fundamentals/Pump.cs
+++ src/TestApp/Fundamentals/Pump.cs
@@ -1,7 +1,36 @@
+using System;
+
 namespace TestApp.Fundamentals;
+
+public interface IPumpService
+{
+    bool Open();
+    bool Close();
+}
+
+public class RealPumpService : IPumpService
+{
+    public bool Open()
+    {
+        Console.WriteLine("Send open signal");
+
+        return true;
+    }
+
+    public bool Close()
+    {
+        Console.WriteLine("Send close signal");
+
+        return true;
+    }
+
+
+}
 
 public class Pump
 {
+    private IPumpService pumpService;
+
     public bool IsRunning { get; private set; }
 
     // Pole określające, czy pompa jest uziemiona
@@ -19,12 +48,19 @@ public class Pump
     private double flowRate;
 
     // Konstruktor który przyjmuje prędkość przepływu
-    public Pump(double flowRate)
+    public Pump(IPumpService pumpService, double flowRate)
     {
         this.isGrounded = false;
         this.flowRate = flowRate;
+        this.pumpService = pumpService;
     }
 
+
+    public Pump(double flowRate)
+        : this(new RealPumpService(), flowRate)
+    {
+
+    }
 
     // Symulacja uziemienia pompy
     public void Ground()
@@ -38,8 +74,14 @@ public class Pump
         if (!IsGrounded)
             throw new System.InvalidOperationException();
 
+
+
         if (isGrounded)
+        {
+            pumpService.Open();
+
             IsRunning = true;
+        }
     }
 
     public void Stop()
